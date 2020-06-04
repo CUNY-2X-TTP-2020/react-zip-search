9ee920c0d8abb206aae5603f60a3e8898c448fce
RickyC0626 <rickychon99@gmail.com>
2020-06-04 14:26:05 -0400

    Create new component just for displaying info

---
+++ src/components/ZipCard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ZipCard extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state =
+        {
+
+        }
+    }
+
+    render()
+    {
+        return (
+            <section>
+
+            </section>
+        );
+    }
+}
+
+ZipCard.propTypes =
+{
+    
+}
